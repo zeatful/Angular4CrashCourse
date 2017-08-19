f4478db4690f2e383cf020481383365ece396c97
ericfoster <eric_foster73@hotmail.com>
2017-08-19 14:12:33 -0400

    ng-template and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { FavoriteChangedEventArgs } from "./favorite/favorite.component";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  courses = [1, 2];
+
   tweet = {
     body: 'Here is the body of a tweet...',
     isLiked: true,
