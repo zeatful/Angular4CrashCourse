f6dca9cfb8301b58300730740f31dd38100547ac
ericfoster <eric_foster73@hotmail.com>
2017-09-02 23:01:30 -0400

    adding observable throw import

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class PostService {
