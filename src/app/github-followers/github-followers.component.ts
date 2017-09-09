9639e76c83bc025380c9c537522b0ef7e48e6ecd
ericfoster <eric_foster73@hotmail.com>
2017-09-08 23:01:53 -0400

    using switchMap to deal with multiple subscribes

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'github-followers',
@@ -18,13 +20,13 @@ export class GithubFollowersComponent implements OnInit {
     Observable.combineLatest([
       this.route.paramMap,
       this.route.queryParamMap
-    ]).subscribe(combined => {
-      let id = combined[0].get('id');
-      let page = combined[1].get('page');
+    ])
+      .switchMap(combined => {
+        let id = combined[0].get('id');
+        let page = combined[1].get('page');
 
-      this.service.getAll()
-        .subscribe(followers => this.followers = followers);
-      //this.service.getAll({id: id, page: page});
-    })
+        return this.service.getAll();
+      })
+      .subscribe(followers => this.followers = followers);
   }
 }
