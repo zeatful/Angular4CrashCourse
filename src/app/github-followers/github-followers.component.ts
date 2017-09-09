8072adef1a2d38aa11fdcf368c1fedc150445770
ericfoster <eric_foster73@hotmail.com>
2017-09-08 22:52:19 -0400

    subsribe to multiple observables

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,8 @@
+import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
   selector: 'github-followers',
@@ -13,15 +15,16 @@ export class GithubFollowersComponent implements OnInit {
   constructor(private route: ActivatedRoute, private service: GithubFollowersService) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ]).subscribe(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
 
-    });
-
-    this.route.queryParamMap.subscribe(params => {
-
-    });
-
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers);
+      this.service.getAll()
+        .subscribe(followers => this.followers = followers);
+      //this.service.getAll({id: id, page: page});
+    })
   }
 }
